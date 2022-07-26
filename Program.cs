f58eb9e26e2647d7f72473c467bed399ecdd035b
Firingo <petr.havlin.0605@gmail.com>
2022-07-26 20:49:04 +0200

    Lekce 3 - Proměnné, typový systém a parsování v C# .NET Opakovací program.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ahoj, jsem virtuální papoušek Lóra, rád opakuji!");
+Console.WriteLine("Napiš něco: ");
+
+string vstup = Console.ReadLine();
+
+string vystup;
+vystup = vstup + ", " + vstup + "!";
+
+Console.WriteLine(vystup);
+Console.ReadKey();
